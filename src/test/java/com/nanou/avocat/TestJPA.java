dccf606a37f6749317ecc821f9547587a2a46a99
danhowser <lahrechamine94@gmail.com>
2016-11-04 14:28:14 +0000

    12

--- src/test/java/com/nanou/avocat/TestJPA.java
+++ src/test/java/com/nanou/avocat/TestJPA.java
@@ -1,7 +1,7 @@
 package com.nanou.avocat;
 
 import static org.junit.Assert.*;
-
+import java.time.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,10 +18,11 @@ public class TestJPA {
 			try {
 			ClassPathXmlApplicationContext context=
 		new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
-			IAvocatMetier metier=(IAvocatMetier) context.getBean("metier");
-			Long l=metier.ajouterUser(new Utilisateur("dab", "h", "r", "g", "e"));
+	IAvocatMetier metier=(IAvocatMetier) context.getBean("metier");
+	Long l = metier.ajouterAvocat(new Avocat( "dab", "h", "r", "g", "e","d","d","h","d",LocalDate.now(),"i","i","o",metier.listVille().get(0)));
 			System.out.println(l);
-			
+//	Utilisateur a=metier.listUser().get(0);
+		//	System.out.println(a);	
 			}
 			catch(Exception e){
 				assertTrue(e.getMessage(),false);
